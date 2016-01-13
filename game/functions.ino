da0479507796b9f5f68f11b30398611127cc49d3
5shekel <yair99@gmail.com>
2016-01-14 00:51:31 +0800

    comments in satges

--- game/functions.ino
+++ game/functions.ino
@@ -1,14 +1,4 @@
-void matrix_circle() {
-  matrix.fillScreen(0);
-  matrix.setCursor(x, 0);
-  matrix.print(F("O"));
-  if (--x < -36) {
-    x = matrix.width();
-    if (++pass >= 3) pass = 0;
-    matrix.setTextColor(colors[pass]);
-  }
-  matrix.show();
-}
+
 
 void BTA_on(int group) {
   int length = 3;
@@ -16,7 +6,7 @@ void BTA_on(int group) {
 
 
   for (int iii = group; iii <= length; iii++)
-    BTA.setPixelColor(iii,BTcolors[1]);
+    BTA.setPixelColor(iii, BTcolors[1]);
 }
 
 void BTB_on(int group) {
@@ -24,8 +14,24 @@ void BTB_on(int group) {
     BTB.setPixelColor(iii, BTcolors[1]);
 }
 
-void flicker_all(int times) {
-  for (int jjj = 0; jjj < times; jjj++) {
+void flicker_ST12(int loop_times, int delay_time) {
+  for (int jjj = 0; jjj < loop_times; jjj++) {
+    for (int iii = 0; iii < pixcount_ST12; iii++)
+      ST12.setPixelColor(iii, BTcolors[0]);
+
+    ST12.show();
+    delay(delay_time);
+
+    for (int iii = 0; iii < pixcount_ST12; iii++)
+      ST12.setPixelColor(iii, BTcolors[1]);
+
+    ST12.show();
+    delay(delay_time);
+  }
+}
+
+void flicker_all(int loop_times, int delay_time) {
+  for (int jjj = 0; jjj < loop_times; jjj++) {
     for (int iii = 0; iii < pixcount_BTB; iii++)
       BTB.setPixelColor(iii, BTcolors[0]);
     for (int iii = 0; iii < pixcount_BTA; iii++)
@@ -46,4 +52,17 @@ void flicker_all(int times) {
     BTB.show(); BTA.show(); ST12.show();
     delay(200);
   }
+}
+
+
+void matrix_circle() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("O"));
+  if (--x < -36) {
+    x = matrix.width();
+    if (++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
 }
--- game/stages.ino
+++ game/stages.ino
@@ -2,7 +2,40 @@
 void stages(int i) {
   switch (i) {
     case 0:
-
+      //                    profit???!
+      break;
+    case 1:
+      //  MATRIX = first icon (magnifying   glass)
+      break;
+    case 2:
+      //MATRIX   = second icon (circle)
+      break;
+    case 3:
+      //BT_4 = on (3leds)
+      break;
+    case 4:
+      //MATRIX   = first icon (magnifying glass)
+      break;
+    case 5:
+      //MATRIX   = second icon (cercle)
+      break;
+    case 6:
+      // BT_7 = on   (3leds)
+      break;
+    case 7:
+      // BT_2 = on           (3leds)
+      break;
+    case 8:
+      //BT_5 = on   (3leds)
+      break;
+    case 9:
+      //BT_3 = on          (3leds)
+      break;
+    case 10:
+      //  BT_6 = on          (3leds)
+      break;
+    case 11:
+      //all lights flick
       break;
     case 12:
       //restart
@@ -10,5 +43,6 @@ void stages(int i) {
       break;
   }
 }
+
 
 
